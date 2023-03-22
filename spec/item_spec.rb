44e0e351719f477e7752e4a57224338dfe0bf477
Shahadat Hossain <shahadat3669@gmail.com>
2023-03-22 19:30:46 +0600

    Add test case for move_to_archive method.

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -14,23 +14,19 @@ describe Item do
   it 'sets a unique id for each new item' do
     item1 = Item.new(Time.now, false)
     item2 = Item.new(Time.now, false)
-
     expect(item1.instance_variable_get(:@id)).not_to eq(item2.instance_variable_get(:@id))
   end
 
   it 'sets the publish date of the item' do
     item = Item.new(Time.now, false)
     new_publish_date = Time.now - 3600
-
     item.publish_date = new_publish_date
-
     expect(item.publish_date).to eq(new_publish_date)
   end
 
   it 'returns the publish date of the item' do
     publish_date = Time.now
     item = Item.new(publish_date, false)
-
     expect(item.publish_date).to eq(publish_date)
   end
 
@@ -48,5 +44,19 @@ describe Item do
   it 'returns false when the item is less than 10 years old' do
     item = Item.new(DateTime.now.prev_year(5).to_time, false)
     expect(item.can_be_archived?).to be false
+  end
+
+  it 'sets the archived flag to true when the item can be archived' do
+    item = Item.new(DateTime.now.prev_year(11).to_time, false)
+    expect(item.instance_variable_get(:@archived)).to eq(false)
+    item.move_to_archive
+    expect(item.instance_variable_get(:@archived)).to eq(true)
+  end
+
+  it 'does not set the archived flag to true when the item cannot be archived' do
+    item = Item.new(DateTime.now.prev_year(5).to_time, false)
+    expect(item.instance_variable_get(:@archived)).to eq(false)
+    item.move_to_archive
+    expect(item.instance_variable_get(:@archived)).to eq(false)
   end
 end
